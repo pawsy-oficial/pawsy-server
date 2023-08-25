8e871c75db2a2ac7dd7351d50dcd61059c4314d2
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-08-25 13:31:31 -0300

    feat: MiddlewareAuthJwtAlmostFinished

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ app.get("/",(req, res)=>{
     res.send("Hello World!")
 })
 
-app.listen(3301, ()=>{
+app.listen(3000, ()=>{
     console.log("server ON");
 })
+
+//APROVEITEI O HORARIO DE ALMOÇO PARA ADIANTAR ALGUMAS COISAS NO MIDDLEWARE, DEPOIS EU CONTINUO E TESTO, EU ENVIO ALGUNS PRINTs PARA VERMOS SE ESTA FUNCIONANDO O JWT!!!
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,27 +1,62 @@
-const express = require('express');
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
 const app = express();
-const bodyParser = require('body-parser');
+const SECRET = 'pawsyruthcardoso'; //Chave secreta
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+app.use(router());
 
-app.get('/clinica', (req, res) => 
-    res.json([{id: $, name: $}])
-)
+// Rota para autenticar e criar um JWT
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
 
-app.get('/tutor', (req, res) => 
-    res.json([{id: $, name: $}])
-)
-
-app.get('/medic', (req, res) => 
-    res.json([{id: $, name: $}])
-)
-
-
-
-app.post('/login', (req,res) => (
-    if(email === queryEmail && password === queryPW){
-        ...
+    // Falta a query do banco, mas verifica se está correto
+    if (email && password) {
+        // Payload que você quer codificar dentro do JWT
+        const payload = {
+            id: id,
+            email: email,
+        };
+        
+        // Criando o JWT
+        const expireHour = 24*30
+        const token = jwt.sign(payload, SECRET, { expiresIn: expireHour });
+        
+        // Definindo o JWT como um cookie
+        res.cookie('token', token, { httpOnly: true }); // httpOnly ApiREST
+        res.status(200).send({ success: true, message: 'Authenticated' });
+    } else {
+        res.status(401).send({ success: false, message: 'Authentication failed' });
     }
-))
+});
 
-//depois eu arrumo e continuo
+// Middleware para verificar JWT
+const verifyJWT = (req, res, next) => {
+    const token = req.cookies.token;
+    
+    if (!token) {
+        return res.status(401).send({ success: false, message: 'No token provided' });
+    }
+    
+    jwt.verify(token, SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ success: false, message: 'Failed to authenticate token' });
+        }
+
+        req.decoded = decoded;  // Coloca o payload decodificado na requisição (opcional)
+        next();
+    });
+};
+
+// Rota que requer autenticação via JWT
+app.get('/autenticacao', verifyJWT, (req, res) => {
+    res.status(200).send({ success: true, message: 'You are authorized!', user: req.decoded });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

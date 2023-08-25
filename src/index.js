63e638b832be192687b4089497e4aa147ba063c5
pedrodanielbrunetto <pedrodanielbm@hotmail.com>
2023-08-25 20:16:12 -0300

    fix: jwtAuthenticated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { verifyJWT, SECRET } = require('./middlewares/auth.js');
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-app.get("/",(req, res)=>{
-    res.send("Hello World!")
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-app.listen(3000, ()=>{
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    //Falta a query para todas as validações
+    
+    if (email && password) {
+        const payload = {
+            id: 777,
+            email: email,
+        };
+        
+        const expireHour = 24*30;
+        const token = jwt.sign(payload, SECRET, { expiresIn: expireHour });
+        
+        res.cookie('token', token, { httpOnly: true }); //httpOnly ajuda na segurança
+        res.status(200).send({ success: true, message: 'Authenticated' });
+    } else {
+        res.status(401).send({ success: false, message: 'Authentication failed' });
+    }
+});
+
+app.get('/autenticacao', verifyJWT, (req, res) => {
+    res.status(200).send({ success: true, message: 'You are authorized!', user: req.decoded });
+});
+
+app.listen(3000, () => {
     console.log("server ON");
-})
-
-//APROVEITEI O HORARIO DE ALMOÇO PARA ADIANTAR ALGUMAS COISAS NO MIDDLEWARE, DEPOIS EU CONTINUO E TESTO, EU ENVIO ALGUNS PRINTs PARA VERMOS SE ESTA FUNCIONANDO O JWT!!!
+});

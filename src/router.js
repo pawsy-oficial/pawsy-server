ba4515f8866e75784686e6dc5be82bdb93ec84a9
rom013 <romullomelo0130@gmail.com>
2023-09-04 15:28:16 -0300

    feat: validation register

--- src/router.js
+++ src/router.js
@@ -7,9 +7,12 @@ router.use(express.json());
 const registerTutor = require("./controllers/registerControllers.js")
 const login = require("./controllers/loginControllers.js")
 
+router.get("/", (req, res)=>{
+    res.status(200).send("Welcome Pawsy")
+});
 router.get("/medico", registerTutor.registerMedic);
 router.get("/clinica", registerTutor.registerClinic);
-router.get("/tutor", registerTutor.registerTutor);
+router.post("/tutor-register", registerTutor.registerTutor);
 
 router.post('/login', login.loginTuto);
 

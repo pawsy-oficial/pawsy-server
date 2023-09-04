a22233ac444493589dcc788db4a32dcb12344a3d
rom013 <romullomelo0130@gmail.com>
2023-09-04 15:28:09 -0300

    feat: validation register

--- src/controllers/registerControllers.js
+++ src/controllers/registerControllers.js
@@ -1,10 +1,21 @@
-const registerTutor = (req, res)=>{
-    return res.status(200).send({ msg: "Cadastro tutor" })
+const yup = require("yup")
+
+const linkSchema = yup.object();
+
+
+
+const registerTutor = (req, res) => {
+    const { firstName, lastName, email, cpf, birthDate, cell, password, cep, city, state, addres, numberHome, complement, neighborhood } = req.body
+    return res.status(200).send({
+        msg: {
+            firstName, lastName, email, cpf, birthDate, cell, password, cep, city, state, addres, numberHome, complement, neighborhood
+        }
+    })
 }
-const registerClinic = (req, res)=>{
+const registerClinic = (req, res) => {
     return res.status(200).send({ msg: "Cadastro clinica" })
 }
-const registerMedic = (req, res)=>{
+const registerMedic = (req, res) => {
     return res.status(200).send({ msg: "Cadastro medico" })
 }
 

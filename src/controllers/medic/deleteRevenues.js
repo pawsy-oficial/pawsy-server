69b9262715b82b7500e656ad353df8976a4c50a5
Fernando <fer.fernandes.moraes05@gmail.com>
2023-11-14 15:14:51 -0300

    fix: deleteRevenues

--- src/controllers/medic/deleteRevenues.js
+++ src/controllers/medic/deleteRevenues.js
@@ -4,7 +4,7 @@ const deleteRevenues = (req, res) => {
     const {idMedic} = req.params
 
     const querySQL = `SELECT id_receita FROM receitas WHERE id_receita = ?`
-    const deleteSQL = `DELETE FROM receitas WHERE id_receita = 1`
+    const deleteSQL = `DELETE FROM receitas WHERE id_receita = ?`
 
     db.query(querySQL, [idMedic], (err, result)=>{
         if(err){
@@ -12,14 +12,14 @@ const deleteRevenues = (req, res) => {
         }
 
         result.length === 0 
-        ? res.status(404).json({message: "medico nao encontrada"})
+        ? res.status(404).json({message: "receita não excluída"})
         : (
             db.query(deleteSQL, [idMedic], (err, result)=>{
                 if(err){
                     res.status(500).json({err})
                 }
 
-                res.status(200).json({message: "medico desativada com sucesso"})
+                res.status(200).json({message: "receita excluída com sucesso"})
             })
         )
     })
